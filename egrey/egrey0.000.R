d55194c912cab54b4f530d517350c97a47c425b8
Ben Tupper <btupper@bigelow.org>
2024-03-05 10:29:20 -0500

    add species_search()

--- egrey/egrey0.000.R
+++ egrey/egrey0.000.R
@@ -6,8 +6,7 @@
 
 
 
-source("setup.R")
-data(mtDNAterms)
+
 
 #' The primary script runner
 #'
@@ -15,7 +14,9 @@ data(mtDNAterms)
 #' @param key char, the entrez key
 main = function(cfg = refdbtools::read_configuration("input/egrey0.000.yaml"),
                 entrez_key = refdbtools::get_entrez_key()){
+  charlier::info("starting version %s", cfg$version)
   
+  charlier::info("taking care of preliminaries")
   rentrez::set_entrez_key(entrez_key)
   
   mtDNAterms = append_mtDNAterms(x = mt_DNAterms(), 
@@ -32,14 +33,23 @@ main = function(cfg = refdbtools::read_configuration("input/egrey0.000.yaml"),
   species_list = readr::read_csv(cfg$species_list$filename, col_types= "c") |>
     rlang::set_names(cfg$species_list$colname)
     
+  if (("subsample" %in% names(cfg$species_list)) && !is.null(cfg$species_list$subsample)) {
+    charlier::info("subsampling the input species_list for development")
+    # subsample for the purpose of development
+    # use the n as the seed to allow this to replicable 
+    set.seed(cfg$species_list$subsample)
+    species_list = dplyr::slice_sample(species_list, n = cfg$species_list$subsample, replace = FALSE)
+  }
+  
   order_list = readr::read_csv(cfg$order_list$filename, col_types = "ccccccc")
   
-  species_list_dedup <- unique(species_list$search_name)
+  species_list_dedup <- unique(species_list[[cfg$species_list$colname]])
   
-  # this runs once per user unless the user specifies \code{overwrite: yes} in the config
-  db_tax_NCBI = taxizedb::db_download_ncbi(verbose = FALSE, 
+  charlier::info("downloading or reading NCBI taxa database")
+  db_tax_NCBI = taxizedb::db_download_ncbi(verbose = cfg$verbose, 
     overwrite = cfg$tax_db$ncbi$overwrite) 
   
+  charlier::info("obtaining fuller taxa classifications for user's species_list")
   # this uses the downloaded db by default and mines a list
   # here we convert it to a data frame which we can operate upon by group (species in this case)
   # I'm saving a copy here, in anticipation of needing restarts later (maybe just for development)
@@ -52,12 +62,17 @@ main = function(cfg = refdbtools::read_configuration("input/egrey0.000.yaml"),
         dplyr::mutate(name = gsub(".", "", .data$name, fixed = TRUE)) |>
         readr::write_csv(taxonomies_cls_filename)
     } 
-  
+    
+  charlier::info("tabulating classification taxa")
   taxa_df = taxonomies_cls |>
     dplyr::mutate(species = factor(.data$species, levels = species_list_dedup)) |>
     tabulate_classification()
     
-  a01_NAMES = dplyr::left_join(species_list, taxa_df,  by = dplyr::join_by(search_name == tax_query))
+    
+  charlier::info("joining taxa classifications for user's species_list")
+  # here we join the users species list with complete taxa found by taxizedb
+  # NONPROGRAMMATIC - see the by= argument
+  a01_NAMES = dplyr::left_join(species_list, taxa_df, by = dplyr::join_by(search_name == tax_query))
   no_species = is.na(a01_NAMES$species)
   a01_NAMES_missing = a01_NAMES |>
     dplyr::filter(no_species)  |>
@@ -66,20 +81,27 @@ main = function(cfg = refdbtools::read_configuration("input/egrey0.000.yaml"),
     dplyr::filter(!no_species) |>
     readr::write_csv("data/a01_NAMES_wTaxonomy.csv.gz")
     
+  
+  charlier::info("searching with entrez_search for cases where order is missing")
   orders_missing = order_list |>
     dplyr::filter(!(order %in% a01_NAMES$order))
+    # this gets us through lines 253 in Erin's
+  #order_seqs = search_order_missing(orders_missing, cfg, verbose = TRUE)
+    
   
-  order_seqs = search_order_missing(orders_missing, cfg, verbose = TRUE)
+    
     
   return(0)
 } # end of main
 
+source("setup.R")
+data(mtDNAterms)
 args = commandArgs(trailingOnly = TRUE)
 cfgfile = if (length(args) <= 0)  "input/egrey0.000.yaml" else args[1]
 keyfile = if (length(args) <= 1) "~/.entrez_key" else args[2]
 cfg = refdbtools::read_configuration(cfgfile)
 entrez_key = refdbtools::get_entrez_key(keyfile)
-
+charlier::start_logger(filename = file.path(cfg$output_folder, sprintf("log-%s", cfg$version)))
 status = main(cfg, entrez_ey = entrez_key)
 
 if (!interactive()) quit(status = status, save = "no")
--- egrey/functions/ncbi.R
+++ egrey/functions/ncbi.R
@@ -1,3 +1,57 @@
+#' Search for species with rentrez::search()
+#'
+#' @param x data frame of species info
+#' @param cfg list, configuartion list
+#' @param target_search_term chr, the target search term
+#' @return
+search_species = function(x, cfg, 
+  target_search_term = "AND (COI OR COX1 OR cox1 OR CO1 OR COXI OR cytochrome c oxidase subunit I OR COX-I OR coi OR MT-CO1 OR mt-Co1 OR mt-co1)"){
+  
+  search_species_one = function(tbl, key, cfg = NULL,
+                                target_search_term = character()){
+
+    search_name <- paste0(tbl$search_name ,cfg$entrez$species_search$search_modifier1)
+    search_term <- paste(search_name, target_search_term, collapse = " ")
+    term = paste(search_name, cfg$entrez$species_search$search_modifier2)
+    mitogenomes =  try(rentrez::entrez_search(db=cfg$entrez$species_search$db, 
+                                             term = term, 
+                                             retmax = cfg$entrez$species_search$retmax))
+      if (!inherits(mitogenomes, "try-error")){
+        n_mitogenomes = mitogenomes$count
+        ids_mitogenomes = paste(mitogenomes$ids, collapse="|")
+        targets = try(rentrez::entrez_search(db=cfg$entrez$species_search$db, 
+                                      term = search_term, 
+                                      retmax = cfg$entrez$species_search$retmax))
+        if (!inherits(targets, "try-error")){
+          n_targets <- targets$count
+          ids_target <- paste(targets$ids, collapse="|")
+        } else {
+          n_targets = NA_real_
+          ids_target = NA_character_
+        }
+        
+      } else {
+        n_mitogenomes = NA_real_
+        ids_mitogenomes = NA_character_
+        n_targets = NA_real_
+        ids_target = NA_character_
+      }
+      Sys.sleep(cfg$entrez$species_search$sleep)
+      dplyr::mutate(tbl,
+        n_mitogenome = n_mitogenomes,
+        ids_mitogenomes = ids_mitogenomes,
+        n_target = n_targets,
+        ids_target = ids_target
+        )
+  }
+  
+  x |>
+    dplyr::rowwise() |>
+    dplyr::group_map(search_species_one, .keep = TRUE, cfg = cfg) |>
+    dplyr::bind_rows()
+}
+
+
 # for each row of orders_missing
 #   species_ids_ls = series of codes
 #   if (the first code is NA) return NULL
@@ -58,6 +112,8 @@ search_order_missing = function(x, cfg,
       species_ids = species_ids_ls
       ss = lapply(species_ids_ls,
         function(id){
+          
+          ### FIRST TRY
           search_name = paste0("txid", id, "[Organism]")
           term1 = paste(search_name, "AND mitochondrion[TITL] AND complete genome[TITL]")
           mitogenomes <- try(rentrez::entrez_search(db=cfg$entrez$order_search1$database, 
@@ -75,13 +131,16 @@ search_order_missing = function(x, cfg,
               Sys.sleep(cfg$entrez$order_search1$sleep)
               return(r)
             } else {
+              ### SECOND TRY
+              if (verbose) cat("mitogenomes search failed with term1")
               term2 = paste(search_name, target_search_term, collapse = " ")
               targets <- try(rentrez::entrez_search(db = cfg$entrez$order_search2$database, 
                                                     term = term2, 
                                                     retmax = cfg$entrez$order_search2$retmax,
                                                     use_history = TRUE))
                 if (!inherits(targets, "try-error")){
-                  if (length(targets[["id"]]) > 0){
+                  cat(str(targets), "\n")
+                  if ((length(targets) > 0) && (length(targets[["id"]]) > 0)){
                     target_id <- sample(as.character(targets[["id"]]),1)
                     r <- dplyr::select(dplyr::all_of(keep)) |>
                       dplyr::mutate(
@@ -91,6 +150,8 @@ search_order_missing = function(x, cfg,
                     Sys.sleep(cfg$entrez$order_search2$sleep)
                     return(r)
                   }
+                } else {
+                  if (verbose)cat("mitogenomes search failed with term2")
                 }
             }  # try targets
           } # mitogenome error?
@@ -124,7 +185,8 @@ search_order_missing = function(x, cfg,
                                                        term = term4, 
                                                        retmax = cfg$entrez$order_search4$retmax,
                                                        use_history = TRUE))
-             if (length(targets[["id"]])>0) {
+                                                       
+             if (!inherits(targets, "try-error") && (length(targets) > 0 ) && (length(targets[["id"]])>0)) {
                target_id = sample(as.character(targets[["id"]]), 1) 
                r <- dplyr::select(dplyr::all_of(keep)) |>
                  dplyr::mutate(
